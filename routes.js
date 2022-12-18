d7010300076c5e97751adc8698b50cb7203117de
Web Dev Solutions <120903253+w-d-s@users.noreply.github.com>
2022-12-18 18:26:20 +0530

    Update routes.js

--- routes.js
+++ routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Schema = require("./Schema");
+const Schema = require("./schema");
 const app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
